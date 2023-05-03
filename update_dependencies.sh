a98983711639afd580d6c752abc17307e2e3150c
luigi-rosso <hello@rive.app>
2023-05-03 21:39:19 +0000

    Fix diacritics
    The big churn is from bumping us to Harfbuzz 6.0.0 (we were on a master from June 2022, 6.0.0 was released December 2022) as it fixes some diacritic positioning with Arabic text.
    
    The main fix, however, comes from marshaling the glyph offsets to Dart (and storing them in C++ which we weren't doing). This is a translation offset within the local space (baseline and advance) of the glyph. It was causing our Arabic diacritics (the "accents" if you will) to be positioned incorrectly.
    
    Prior to the fix:
    ![image](https://user-images.githubusercontent.com/454182/235834041-a683851a-96b0-4c72-adb9-f68c97682c14.png)
    
    Post fix:
    ![image](https://user-images.githubusercontent.com/454182/235834074-23db8fab-08be-4529-98b3-f8a04ebe623a.png)
    
    Same word displayed in Google Fonts (confirming correctness, also confirmed by an Arabic reader  on our Discord):
    ![CleanShot 2023-05-02 at 21 43 33@2x](https://user-images.githubusercontent.com/454182/235834273-9e423c7f-9dc2-48c3-afec-c7f7a9986416.png)
    
    Diffs=
    a8c1e3e75 Fix diacritics (#5200)
    c7ecbe025 Run clang-format on .glsl files (#5180)
    9945a7d2d Merge sbs_render_and_advance into master (#5181)
    542e320d2 Discard empty verbs from RawPath (#5166)

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -59,12 +59,12 @@ function installRiveCpp {
         echo "Cloning Harfbuzz."
         git clone https://github.com/harfbuzz/harfbuzz
         pushd harfbuzz
-        git checkout 858570b1d9912a1b746ab39fbe62a646c4f7a5b1 .
+        git checkout "6.0.0" .
         popd
     fi
     if [ $FORCE == "true" ] || [ ! -d SheenBidi ]; then
         rm -fR SheenBidi
-        echo "Cloning Harfbuzz."
+        echo "Cloning SheenBidi."
         git clone https://github.com/Tehreer/SheenBidi.git
         pushd SheenBidi
         git checkout v2.6 .
