c321dce473a54f47b5c88a388203e058610b219c
Marco Papula <papulamarco@gmail.com>
2023-07-11 09:11:37 +0200

    add mock asset bundle

--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rive/src/rive_core/artboard.dart';
 export 'fakes.dart';
@@ -8,3 +9,5 @@ abstract class _OnInitFunction {
 }
 
 class OnInitCallbackMock extends Mock implements _OnInitFunction {}
+
+class MockAssetBundle extends Mock implements AssetBundle {}
