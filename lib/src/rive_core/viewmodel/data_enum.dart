5ae54033750e9821135feaaec5e81b2d8efb4ef2
bodymovin <hello@rive.app>
2024-10-30 20:56:43 +0000

    add viewmodel and data enum system core objects
    This PR only has the boilerplate code to support system data enums.
    System enums will be enums that are built-in in Rive, like text alignment, word wrapping, blend modes.
    Those system enums won't be included in the .riv file, but they'll exist built-in in the runtime.
    This PR creates 4 new core objects types. 2 extends from data_enum and 2 from viewmodel_property_enum.
    The rest of the changes are renaming the base class to the custom one.
    
    Diffs=
    5297cf0cc2 add viewmodel and data enum system core objects (#8382)
    f53d2d9c91 editor: bug fixes for image n-slicing (#8424)
    2c83c60225 Nnnn fix layout hittest order (#8435)
    afcf0992da editor: vector n-slicer should clamp all shapes contained (#8428)
    c7de07c0ca fix for image_diff not correctly outputting newline in status file (#8438)
    ea59076bf7 Expose RenderContext methods for allocating tessellation vertices (#8408)
    e71b4cc081 feat: add runtime layout fit type for ios, android, web (#8341)
    
    Co-authored-by: hernan <hernan@rive.app>

--- lib/src/rive_core/viewmodel/data_enum.dart
+++ lib/src/rive_core/viewmodel/data_enum.dart
@@ -1,34 +1,11 @@
 import 'package:rive/src/generated/viewmodel/data_enum_base.dart';
-import 'package:rive/src/rive_core/container_component.dart';
-import 'package:rive/src/rive_core/viewmodel/data_enum_value.dart';
 
 export 'package:rive/src/generated/viewmodel/data_enum_base.dart';
 
 class DataEnum extends DataEnumBase {
-  DataEnumValues children = DataEnumValues();
-  void addValue(DataEnumValue item) {
-    if (!children.contains(item)) {
-      children.add(item);
-    }
-  }
-
   @override
   void onAdded() {}
 
   @override
   void onAddedDirty() {}
-
-  @override
-  void onRemoved() {
-    children.toList().forEach((child) => child.remove());
-    super.onRemoved();
-  }
-
-  void removeValue(DataEnumValue item) {
-    children.remove(item);
-  }
-
-  void internalAddValue(DataEnumValue item) {
-    addValue(item);
-  }
 }
